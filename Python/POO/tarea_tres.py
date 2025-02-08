98283f365ab9ecbef04855bdbe9efef08f164d64
Nihil-VoidZeroNull <moreno.gab58@gmail.com>
2025-02-08 14:30:00 -0400

    ...

---
+++ Python/POO/tarea_tres.py
@@ -0,0 +1,15 @@
+"""_Tarea 3_
+    Un medigo que entre a cirugia:
+    medico,
+    especialidad,
+    operando
+"""
+
+class Medico:
+    def __init__(self):
+        self.nombre = None
+        self.apellido = None
+        self.telefono = None
+        self.direccion = None
+        self.especialidad = None
+        self.operando = False
